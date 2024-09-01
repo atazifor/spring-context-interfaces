2f8f31156689b3b6db0e6bf4eb9595a288fd5b0e
Amin Tazifor <amin.tazifor@gmail.com>
2024-09-01 09:16:14 +0100

    Adding a second implementation of CommentnotificationProxy will throw a No qualifying bean of type 'proxies.CommentNotificationProxy' because spring does not know which of the 2 implementations to use.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,17 @@
 package main;
 
+import config.ProjectConfig;
 import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import proxies.EmailCommentNotificationProxy;
 import repositories.DBCommentRepository;
 import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        DBCommentRepository commentRepository = new DBCommentRepository();
-        EmailCommentNotificationProxy commentNotificationProxy = new EmailCommentNotificationProxy();
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        CommentService commentService = new CommentService(commentRepository, commentNotificationProxy);
+        CommentService commentService = applicationContext.getBean(CommentService.class);
 
         Comment comment = new Comment();
         comment.setAuthor("Taz");
--- src/main/java/proxies/PushCommentNotificationProxy.java
+++ src/main/java/proxies/PushCommentNotificationProxy.java
@@ -4,9 +4,9 @@ import model.Comment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+public class PushCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Email notification for comment: " + comment.getText());
+        System.out.println("Push notification for comment: " + comment.getText());
     }
 }
